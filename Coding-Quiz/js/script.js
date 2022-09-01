d9aa7e5f68620ecad3bef29ad15b3a97cfc557c2
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-09-01 15:24:20 -0400

    debugging

--- Coding-Quiz/js/script.js
+++ Coding-Quiz/js/script.js
@@ -3,20 +3,34 @@ const rulesBox = document.querySelector(".rules-container");
 const exitBtn = document.querySelector(".exit");
 const continueBtn = document.querySelector(".continue");
 const quizBox = document.querySelector(".quiz-container");
+const resultsBox = document.querySelector(".results-container");
+const restartQuiz = document.querySelector(".restart");
+const quitQuiz = document.querySelector(".quit");
+let timeValue =  15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
 
 // Prompt Rules
 startBtn.addEventListener("click", () => {
   rulesBox.classList.add("showRules");
-  console.log("hello");
 });
 
 // Exit
 exitBtn.addEventListener("click", () => {
-    rulesBox.classList.remove("showRules");
-})
+  rulesBox.classList.remove("showRules");
+});
 
 // Start Quiz
 continueBtn.addEventListener("click", () => {
-    rulesBox.classList.remove("showRules");
-    quizBox.classList.add("showQuiz")
-})
+  rulesBox.classList.remove("showRules");
+  quizBox.classList.add("showQuiz");
+  showQuestions(0);
+  queCounter(1);
+  startTimer(15);
+  startTimeLine(0);
+});
