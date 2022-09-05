f0ec0ea227b8b9d25a6473f86ac06dc4e116bb65
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-09-05 10:27:15 -0400

    rules buttons work, quiz shows up on screen

--- Coding-Quiz/js/script.js
+++ Coding-Quiz/js/script.js
@@ -6,14 +6,64 @@ const quizBox = document.querySelector(".quiz-container");
 const resultsBox = document.querySelector(".results-container");
 const restartQuiz = document.querySelector(".restart");
 const quitQuiz = document.querySelector(".quit");
-let timeValue =  15;
-let que_count = 0;
-let que_numb = 1;
-let userScore = 0;
-let counter;
-let counterLine;
-let widthValue = 0;
+// let timeValue = 15;
+// let que_count = 0;
+// let que_numb = 1;
+// let userScore = 0;
+// let counter;
+// let counterLine;
+// let widthValue = 0;
 
+// Questions
+let questions = [
+  {
+    id: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+    id: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+    id: 3,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+    id: 4,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+    id: 5,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+];
 
 // Prompt Rules
 startBtn.addEventListener("click", () => {
@@ -29,8 +79,8 @@ exitBtn.addEventListener("click", () => {
 continueBtn.addEventListener("click", () => {
   rulesBox.classList.remove("showRules");
   quizBox.classList.add("showQuiz");
-  showQuestions(0);
-  queCounter(1);
-  startTimer(15);
-  startTimeLine(0);
+//   showQuestions(0);
+//   queCounter(1);
+//   startTimer(15);
+//   startTimeLine(0);
 });
