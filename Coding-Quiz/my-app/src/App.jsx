6bb310ffe6bab1913ef77f0dc468900859476ecc
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-09-19 14:47:25 -0400

    new direction

---
+++ Coding-Quiz/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./components/main/Main"
+import Pyramid from "./components/pyramid/Pyramid"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="container">
+      <Main />
+      <Pyramid />
+    </div>
+  );
+}
+
+export default App;
